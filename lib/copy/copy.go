b7156f2a5efdb197e3e62392c51fdc14d3d37eca
FObersteiner <f.obersteiner@posteo.de>
2023-08-22 21:22:02 +0200

    move to separate folder

---
+++ lib/copy/copy.go
@@ -0,0 +1,49 @@
+package copy
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+const BUFFERSIZE = 4096
+
+func CopyFile(src, dst string) error {
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	buf := make([]byte, BUFFERSIZE)
+	for {
+		n, err := source.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		if _, err := destination.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CopyPerm tries to copy permissions from src to dst file
+func CopyPerm(src, dst string) error {
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	log.Println(srcStat.Mode())
+
+	return os.Chmod(dst, srcStat.Mode())
+}
