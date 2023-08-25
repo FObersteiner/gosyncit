416d0e49f9c65a98242fafa62683d3fd4e267be2
FObersteiner <f.obersteiner@posteo.de>
2023-08-25 12:57:44 +0200

    add test for copy permissions
    not sure how this works on non-linux OS

--- lib/copy/copy_test.go
+++ lib/copy/copy_test.go
@@ -5,12 +5,19 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"syscall"
 	"testing"
 
 	cp "gosyncit/lib/copy"
 )
 
 func TestCopyFile(t *testing.T) {
+	// remove umask so that arbitrary permissions can be specified
+	// TODO : does this work on !linux ?!
+	oldumask := syscall.Umask(0000)
+	defer syscall.Umask(oldumask)
+
 	dirA, err := os.MkdirTemp("", "dirA")
 	if err != nil {
 		log.Fatal(err)
@@ -24,9 +31,12 @@ func TestCopyFile(t *testing.T) {
 	defer os.RemoveAll(dirB)
 
 	file := filepath.Join(dirA, "tmpfileA")
-	if err := os.WriteFile(file, []byte("content"), 0644); err != nil {
+	if err := os.WriteFile(file, []byte("content"), 0655); err != nil {
 		log.Fatal(err)
 	}
+
+	fstSrc, _ := os.Stat(file)
+	// log.Println(fstSrc.Mode())
 
 	dst := filepath.Join(dirB, "tmpfileA")
 	err = cp.CopyFile(file, dst)
@@ -47,5 +57,22 @@ func TestCopyFile(t *testing.T) {
 	if !bytes.Equal(f1, f2) {
 		t.Logf("Expected %x, got %x", f1, f2)
 		t.Fail()
+	}
+
+	// fstDst, _ := os.Stat(dst)
+	// log.Printf("%o\n", fstDst.Mode())
+
+	if runtime.GOOS != "windows" {
+		err = cp.CopyPerm(file, dst)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fstDst2, _ := os.Stat(dst)
+		// log.Println(fstDst2.Mode())
+
+		if fstSrc.Mode() != fstDst2.Mode() {
+			t.Logf("Expected permission %o, got %o", fstSrc.Mode(), fstDst2.Mode())
+			t.Fail()
+		}
 	}
 }
