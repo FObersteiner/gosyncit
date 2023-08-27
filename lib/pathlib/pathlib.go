beab5733fab4bdfa5cdb3f4baaafd74f149a8ef8
FObersteiner <f.obersteiner@posteo.de>
2023-08-27 18:58:25 +0200

    revise functions

--- lib/pathlib/pathlib.go
+++ lib/pathlib/pathlib.go
@@ -2,18 +2,40 @@ package pathlib
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
-func CheckPath(path string) (string, error) {
+// CheckSrcDst cleans input path strings 'src' and 'dst'. Src and dst must not be identical,
+// src must exist but dst not necessarily.
+func CheckSrcDst(src, dst string) (string, string, error) {
+	srcCleaned, err := CheckDirPath(src)
+	if err != nil {
+		return "", "", err
+	}
+	dstCleaned, err := CheckDirPath(dst)
+	// If dst directory does not exist, that is not considered an error.
+	if err != nil && !os.IsNotExist(err) {
+		return "", "", err
+	}
+	if src == dst {
+		return "", "", errors.New("src and dst must not be identical")
+	}
+	return srcCleaned, dstCleaned, nil
+}
+
+// CheckDirPath cleans a path and checks for its existence.
+// Files are considered invalid, must be directory.
+// Non-existing directories are considered an error here.
+func CheckDirPath(path string) (string, error) {
 	path = filepath.Clean(path)
 	stats, err := os.Stat(path)
 	if err != nil {
 		return "", err
 	}
 	if !stats.IsDir() {
-		return "", errors.New("specified directory is a file")
+		return "", fmt.Errorf("specified '%v' directory is a file", path)
 	}
 	return path, nil
 }
--- lib/pathlib/pathlib_test.go
+++ lib/pathlib/pathlib_test.go
@@ -13,12 +13,51 @@ func TestCheckPath(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	cleaned, err := pathlib.CheckPath(tmp)
+	cleaned, err := pathlib.CheckDirPath(tmp)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if tmp != cleaned {
 		t.Log("auto-generated directory path must be clean")
+		t.Fail()
+	}
+
+	f, err := os.CreateTemp("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+	_, err = pathlib.CheckDirPath(f.Name())
+	if err == nil {
+		t.Log("path to a file must give an error; only paths allowed")
+		t.Fail()
+	}
+}
+
+func TestSrcDstCheck(t *testing.T) {
+	_, _, err := pathlib.CheckSrcDst("nonexisting", "")
+	if !os.IsNotExist(err) {
+		t.Logf("expected non-existing dir error, got %v", err)
+		t.Fail()
+	}
+
+	tmp, err := os.MkdirTemp(os.TempDir(), "test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, _, err = pathlib.CheckSrcDst(tmp, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.CreateTemp("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+	_, _, err = pathlib.CheckSrcDst(tmp, f.Name())
+	if err == nil {
+		t.Log("path to a file must give an error; only paths allowed")
 		t.Fail()
 	}
 }
