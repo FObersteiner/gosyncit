d8d9125cae7ce02085f80f4f7df8c7b7fac17859
FObersteiner <florian.obersteiner@kit.edu>
2023-10-26 16:53:45 +0200

    actual mirror code to wrapper func

--- cmd/sftpmir.go
+++ cmd/sftpmir.go
@@ -1,23 +1,7 @@
 /*
 Copyright © 2023 Florian Obersteiner <f.obersteiner@posteo.de>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+License: see LICENSE in the root directory of the repo.
 */
 package cmd
 
@@ -129,18 +113,24 @@ func init() {
 	}
 }
 
-// SftpMir mirrors directory 'local' to 'remote' via SFTP
+// SftpMir mirrors directory 'local' to 'remote' (SFTP) or vice versa.
 func SftpMir(local, remote string, creds libsftp.Credentials, reverse, dry, ignorehidden, clean bool) error {
-	//
-	// TODO : handle non-existing cases for local, remote
-	// TODO : handle reverse-case: mirror remote to local
-	//
 	fmt.Println("~~~ SFTP MIRROR ~~~")
 	verboseprintf("%s\n", &creds)
-	fmt.Printf("'%s' --> '%s'\n\n", local, remote)
+	arrow := "-->"
+	if reverse {
+		arrow = "<--"
+	}
+	fmt.Printf("'%s' %v '%s'\n\n", local, arrow, remote)
 
-	fmt.Println("reverse:", reverse, "| dry:", dry, "| ignorehidden", ignorehidden)
+	if reverse {
+		return sftpRemoteToLocal(local, remote, creds, dry, ignorehidden, clean)
+	}
+	return sftpLocalToRemote(local, remote, creds, dry, ignorehidden, clean)
+}
 
+// wrapper for default mirror case
+func sftpLocalToRemote(local, remote string, creds libsftp.Credentials, dry, ignorehidden, clean bool) error {
 	var nItems, nBytes uint
 	t0 := time.Now()
 
@@ -181,10 +171,6 @@ func SftpMir(local, remote string, creds libsftp.Credentials, reverse, dry, igno
 		verboseprint("remote fileset population got error", err)
 		return err
 	}
-
-	// verboseprintf("local %v\n", filesetLocal.String())
-	// verboseprintf("local %v\n", filesetRemote.String())
-
 	basepath := strings.TrimSuffix(filesetLocal.Basepath, string(os.PathSeparator))
 
 	// step 1: copy everything from source to dst if src newer
@@ -292,6 +278,10 @@ func SftpMir(local, remote string, creds libsftp.Credentials, reverse, dry, igno
 		copy.ByteCount(nBytes),
 		dt,
 	)
+	return nil
+}
 
+// wrapper if reverse == True
+func sftpRemoteToLocal(local, remote string, creds libsftp.Credentials, dry, ignorehidden, clean bool) error {
 	return nil
 }
